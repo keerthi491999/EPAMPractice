600722f08807dd02a6f7d0d2a8bcf2ec4e8c7273
Keerthi Nandigam <36662407+keerthi491999@users.noreply.github.com>
2019-12-23 15:58:53 +0530

    Add files via upload

---
+++ Milestone1.1.java
@@ -0,0 +1,6 @@
+class CmdArgs{
+	public static void main(String[] args) {
+		if(args[0].equals("Wipro") && args[1].equals("Bangalore"))
+			System.out.println("Wipro Technologies Bangalore");
+	}
+}
---
+++ Milestone1.2.java
@@ -0,0 +1,5 @@
+class CmdArgs{
+	public static void main(String[] args) {
+		System.out.println("Hello "+args[0]);
+	}
+}
